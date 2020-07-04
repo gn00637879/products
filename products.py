1dc0984752ce04f412b82ae28cf957653cd31908
Rex Chang <gn00637879@gmail.com>
2020-07-04 20:56:55 +0800

    add csv

--- products.py
+++ products.py
@@ -5,8 +5,13 @@ while True:
 		break
 	price = input('請輸入價格: ')
 	products.append([name, price])
-print(products)
+#print(products)
 for p in products:
 	print('商品名稱:', p[0], ',價格為:', p[1])
+
+with open('products.csv', 'w') as f:
+	f.write('商品名稱,價格'+'\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
 
 
